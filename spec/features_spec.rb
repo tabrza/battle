c855bf9e212ba7f65ff532f996b2763cc2f902b1
Joe Cowton <joecowton@gmail.com>
2017-11-08 14:37:49 +0000

    players can input names

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,8 +1,19 @@
 require 'battle'
 
 feature '#homepage', :type => :feature do
-  it 'returns testing infrastructure' do
+  scenario 'returns testing infrastructure' do
     visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
+    # expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
+
+feature '#player names', :type => :feature do
+  scenario 'players can enter names' do
+    visit '/'
+    fill_in :player_1_name, with: "John"
+    fill_in :player_2_name, with: "Doe"
+    click_button 'Submit'
+    # save_and_open_page
+    expect(page).to have_content 'John vs. Doe'
   end
 end
