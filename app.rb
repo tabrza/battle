cfb15c840be9dbda4fb1d6f2d5e54e85a56b5540
Tabish Raza <tabishraza@gmail.com>
2017-11-09 10:39:18 +0000

    using global variable for player names

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -9,14 +10,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player1 = Player.new(params[:player_1_name])
+    $player2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     @player_1_HP = 100
     @player_2_HP = 100
     @attack_confirmation = session[:attack_confirmation]
@@ -24,13 +25,9 @@ class Battle < Sinatra::Base
   end
 
   post '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
-    session[:attack_confirmation] = "#{@player_2_name} Attacked"
+    session[:attack_confirmation] = "#{$player2.name} Attacked"
     redirect '/play'
   end
-
-
 
   #start the server if ruby file executed directly
   run! if app_file == $0
